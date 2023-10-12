cf26a7dc1212081e0d6df7aa17ba917dc515eaab
LisaMaatje <51792569+LisaMaatje@users.noreply.github.com>
2023-10-12 14:09:33 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,10 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// Gebruik `workbox.routing.registerRoute()`...
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'EEEN_CACHE_KEY'
+  }) 
+)
 
